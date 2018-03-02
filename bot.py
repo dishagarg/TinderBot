520005c0e14737d8e0962aab38e798a2dc159a3a
Disha Garg <garg.disha8@gmail.com>
2018-03-01 20:06:58 -0800

    Updating liking filter

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import fb_auth_token
 import tinder_api
 import config
+import re
+import json
 
 fb_access_token = fb_auth_token.get_fb_access_token(config.fb_username, config.fb_password)
 fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
@@ -13,5 +15,47 @@ print fb_user_id
 
 tinder_auth_token = tinder_api.get_auth_token(fb_access_token, fb_user_id)
 print tinder_auth_token
-print tinder_api.authverif(fb_access_token, fb_user_id)
-print tinder_api.get_self()
+# print tinder_api.authverif(fb_access_token, fb_user_id)
+# print tinder_api.get_self()
+
+
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+person_id=''
+rating = 0
+
+def likeAPerson():
+    for key, value in tinder_api.get_recommendations().items():
+        if(key == "results"):
+            for person in value:
+                rating = 0
+                for key, value in person.items():
+                    if(key == "_id"):
+                        person_id = value
+                        print person_id
+                    if(key == "photos" and len(value) > 1):
+                        print "photos OK!"
+                        rating += 1
+                    if((key=="bio") and value!=""):
+                        rating += 1
+                        if(any(word in value for word in config.desired_job)):
+                            print value.encode('utf-8', 'replace')
+                            rating += 1
+                    if((key=="teasers") and len(value) > 1):
+                        rating += 1
+                    if((key=="jobs") and len(value) > 1):
+                        rating += 1
+                if rating>=3:
+                    print tinder_api.like(person_id)
+        
+
+likeAPerson()
+
+#matches = tinder_api.get_updates()['matches']
+#
+#with open('data.txt', 'w') as outfile:
+#    json.dump(matches, outfile)
+
+#ms = tinder_api.send_msg(config.match_id, config.msg)
+#print ms
